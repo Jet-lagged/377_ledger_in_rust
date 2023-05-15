063ee9e1377aa29ad795e5d767b7a6824f7fbe26
Tim Quach <persontim11@gmail.com>
2023-05-15 18:09:56 -0400

    test in main for bank base complete

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,14 @@ fn main() {
     let mut bank = Bank::new(5);
 
     bank.deposit(0, 0, 0, 300);
-    bank.print_account();
     bank.withdraw(0, 1, 0, 200);
-    bank.print_account();
     bank.withdraw(0, 1, 0, 200);
+
+    
+    bank.deposit(0, 2, 1, 500);
+    // 0 has 100, 1 has 500
+    bank.print_account();
+    bank.transfer(0, 3, 1, 0, 300);
+    // 0 has 400, 1 has 200
     bank.print_account();
 }
